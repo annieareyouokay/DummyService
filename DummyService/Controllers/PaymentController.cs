eacf6be5bbfe13674ab1edd301cc2f5cf9853b57
Maksim Yurovnik <yurovnik.mv@dns-shop.ru>
2024-11-28 15:52:49 +1000

    Добавьте файлы проекта.

---
+++ DummyService/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost("payment")]
+        public async Task<ActionResult<bool>> CompletePaymentAsync(CancellationToken cancellationToken)
+        {
+            return await Task.Run(() => 
+            {
+                Random random = new Random();
+                return Ok(random.Next(0, 100) > 50);
+            });
+        }
+    }
+}
